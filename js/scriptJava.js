a0e1a3d9d15d38174510c3094c21781ad1f2d511
gandalfs <franklinortiz93@hotmail.com>
2022-05-06 21:19:02 -0500

    Se ajusta las cajas de texto y el js
    *se agrega name a las cjas de texto en index.html
    *se modifica hasta donde llega el form para que ahora el boton haga submit
    *en el js se ajusta para poder capturar los datos y mostrarlos en consola

--- js/scriptJava.js
+++ js/scriptJava.js
@@ -1,6 +1,7 @@
 const openModal = document.getElementById('openRegisterModal')
 const modal = document.getElementById('modal')
 const closerModal = document.getElementById('closeRegisterModal')
+const registerForm = document.getElementById('register-form')
 
 const showRegisterModal = () =>{
     modal.classList.toggle('is-active')
@@ -8,3 +9,13 @@ const showRegisterModal = () =>{
 
 openModal.addEventListener('click',showRegisterModal)
 closerModal.addEventListener('click',showRegisterModal)
+
+registerForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const codigo = registerForm['codigo'].value
+    const precioUnitario = registerForm['precioUnitario'].value
+    const cantidad = registerForm['cantidad'].value
+    const precioTotal = registerForm['precioTotal'].value
+    const detalle = registerForm['detalle'].value
+    console.log(codigo, precioUnitario, cantidad, precioTotal, detalle)
+})
