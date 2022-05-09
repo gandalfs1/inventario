180abeae1a5c72c931627c35c6525038e7a9a0ca
gandalfs <franklinortiz93@hotmail.com>
2022-05-08 22:06:40 -0500

    Se ajusta lectura de datos
    *se agrega codigo para leer desde firebase y pintarlos en la tabla

--- js/scriptJava.js
+++ js/scriptJava.js
@@ -14,14 +14,15 @@ const closerModal = document.getElementById('closeRegisterModal')
 const registerForm = document.getElementById('register-form')
 firebase.initializeApp(firebaseConfig)
 const productRef = firebase.database().ref('products');
-const db = firebase.firestore();
+var database = firebase.database();
+
 const showRegisterModal = () => {
   modal.classList.toggle('is-active')
 }
 
 openModal.addEventListener('click', showRegisterModal)
 closerModal.addEventListener('click', showRegisterModal)
-
+//! insertar datos
 registerForm.addEventListener('submit', (e) => {
   e.preventDefault()
   const codigo = registerForm['codigo'].value
@@ -32,7 +33,7 @@ registerForm.addEventListener('submit', (e) => {
 
   const registerProduct = productRef.push()
   registerProduct.set({
-    Uid: registerProduct.toString().substring(firebase.database().ref().toString().length-1),
+    Uid: registerProduct.toString().substring(firebase.database().ref().toString().length - 1),
     Codigo: codigo,
     PrecioUnitario: precioUnitario,
     Cantidad: cantidad,
@@ -40,4 +41,43 @@ registerForm.addEventListener('submit', (e) => {
     Detalle: detalle
   })
   showRegisterModal()
+})
+
+//! leer datos de registros
+
+window.addEventListener('DOMContentLoaded', async (e) => {
+  await productRef.on('value', (products) => {
+    const productsTable = document.getElementById('products-table');
+    productRef.get().then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+        productsTable.innerHTML = ''
+        productRef.once("value")
+        .then(function(snapshot) {
+          snapshot.forEach(function(childSnapshot) {
+            console.log(snapshot.child(childSnapshot.key).child("Codigo").val());
+            productsTable.innerHTML +=`<tr>
+            <th>${snapshot.child(childSnapshot.key).child("Codigo").val()}</th>
+            <td>${snapshot.child(childSnapshot.key).child("Detalle").val()}</td>
+            <td>${snapshot.child(childSnapshot.key).child("PrecioUnitario").val()}</td>
+            <td>${snapshot.child(childSnapshot.key).child("PrecioTotal").val()}</td>
+            <td>${snapshot.child(childSnapshot.key).child("Cantidad").val()}</td>
+            <td>
+              <button class="button is -warning" data-id="">
+                <i class="fas fa-pencil-alt"></i>
+              </button>
+              <button class="button is-danger" data-id="">
+                <i class="fas fa-trash"></i>
+              </button>
+            </td>
+          </tr>`         
+        });
+      });
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  })
 })
